82d7b03694cdb1c6a7b1f8eda00999b4eb64fd49
nilavalagansugumaran <Nilavalagan_s@yahoo.com>
2021-02-02 10:50:52 +0000

    Demo classes and objects

--- demo/DemoClassesObjects/src/com/company/Main.java
+++ demo/DemoClassesObjects/src/com/company/Main.java
@@ -9,14 +9,23 @@ public class Main {
 
         // To create an object
         BankAccount bankAccount = new BankAccount();
-        // to check default intialisation
+        // to check default initialisation
         System.out.println(bankAccount.toString());
+        System.out.println(BankAccount.getCounter()); // accessing static methods using class name
 
         //using setters
-        bankAccount.setAccountNumber(101);
-        bankAccount.setAccountHolderName("Nila");
-        bankAccount.deposit(10);
-        System.out.println(bankAccount.toString());
+        BankAccount bankAccount1 = new BankAccount();
+       // bankAccount1.setAccountNumber(101); - set using class variable called counter
+        bankAccount1.setAccountHolderName("Nila");
+        //access methods
+        bankAccount1.deposit(10);
+        System.out.println(bankAccount1.toString());
+        System.out.println(BankAccount.getCounter()); // accessing static methods using class name
+
+        //using constructor
+        BankAccount bankAccount2 = new BankAccount("my name", 200);
+        System.out.println(bankAccount2.toString());
+        System.out.println(BankAccount.getCounter()); // accessing static methods using class name
 
     }
 }
--- demo/DemoClassesObjects/src/com/company/domain/BankAccount.java
+++ demo/DemoClassesObjects/src/com/company/domain/BankAccount.java
@@ -11,8 +11,49 @@ public class BankAccount {
     private int accountNumber; // 0
     private Date accountCreationDate = new Date();
 
+    // class variable
+    private static int counter = 100;
+
+    // Constructors - no return type
+    // used for initialising values
+    public BankAccount(){
+        System.out.println("default constructor has been invoked" );
+        this.accountHolderName = "Anonymous";
+        this.accountNumber = ++counter;
+    }
+
+    public BankAccount(String accountHolderName, double balance){
+        System.out.println("parameterised constructor has been invoked" );
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+        this.accountNumber = ++counter;
+    }
+
+    // initialisation block - executed everytime new object is created, can appear anywhere, executed as per the order it appears in the class
+    {
+        System.out.println("initialisation block 1" );
+    }
+    // initialisation block - executed everytime new object is created, can appear anywhere, executed as per the order it appears in the class
+    {
+        System.out.println("initialisation block 2" );
+    }
+
+    // static initialisation block - executed only once when the class is loaded
+    static {
+        System.out.println("static initialisation block 1" );
+    }
+
     // instance methods or behaviors
     public void deposit(double amountToDeposit){
+
+        System.out.println("depositing :: " + amountToDeposit);
+        balance = balance + amountToDeposit;
+        System.out.println("new balance  :: " + balance);
+        // return statement is not needed because it is void
+    }
+
+    // overloading
+    public void deposit(double amountToDeposit, String currency){
 
         System.out.println("depositing :: " + amountToDeposit);
         balance = balance + amountToDeposit;
@@ -61,6 +102,26 @@ public class BankAccount {
         this.accountCreationDate = accountCreationDate;
     }
 
+    // initialisation block - executed everytime new object is created, can appear anywhere, executed as per the order it appears in the class
+    {
+        System.out.println("initialisation block 3" );
+    }
+
+    // static initialisation block - executed only once when the class is loaded
+    static {
+        System.out.println("static initialisation block 2" );
+    }
+
+
+    // initialisation block - executed everytime new object is created, can appear anywhere, executed as per the order it appears in the class
+    {
+        System.out.println("initialisation block 4" );
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
     @Override
     public String toString() {
         return "BankAccount{" +
@@ -68,6 +129,7 @@ public class BankAccount {
                 ", balance=" + balance +
                 ", accountNumber=" + accountNumber +
                 ", accountCreationDate=" + accountCreationDate +
+                ", counter=" + counter +
                 '}';
     }
 }
