b0875e84da2bc4da21b97da238f05f1d2ef97311
nilavalagansugumaran <Nilavalagan_s@yahoo.com>
2021-02-04 13:45:16 +0000

    Autowire

--- springDemos/demoBeans/src/main/java/com/example/demobeans/BankServiceImpl.java
+++ springDemos/demoBeans/src/main/java/com/example/demobeans/BankServiceImpl.java
@@ -6,16 +6,23 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 //@Scope("prototype") // TO ADD SCOPE
 @Lazy // to delay bean creation
 public class BankServiceImpl implements BankService{
 
     //Dependency Injections
-    @Autowired // property injection - way1
+    @Autowired(required = false) // property injection - way1
    // @Qualifier("prodrepo") // use to choose from multiple beans
     @Qualifier("bankRepositoryImpl")
-    private BankRepository repository;
+    private BankRepository repository; // define default = new BankRepositoryImpl();
+
+    //Dependency Injections
+    @Autowired(required = false) // property injection as list - way3
+    private List<BankRepository> repositoryList;
+
 
 //    @Autowired // constructor DI - way 2 (preferred)
 //    public BankServiceImpl(BankRepository repository){
@@ -28,6 +35,11 @@ public class BankServiceImpl implements BankService{
         //repository = new BankRepositoryImpl(); - not required as this is not a Spring way of dealing with beans
         repository.updateTable();
         System.out.println("taking deposit complete at BankServiceImpl....");
+
+        // process the list of beans
+        for(BankRepository repo: repositoryList) {
+            repo.updateTable();
+        }
     }
 
     @Override
