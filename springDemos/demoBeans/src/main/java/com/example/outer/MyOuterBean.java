bbcd012922e6064c31804edc73fc4af3df41cbab
nilavalagansugumaran <Nilavalagan_s@yahoo.com>
2021-02-04 13:53:48 +0000

    property value injections

--- springDemos/demoBeans/src/main/java/com/example/outer/MyOuterBean.java
+++ springDemos/demoBeans/src/main/java/com/example/outer/MyOuterBean.java
@@ -1,12 +1,22 @@
 package com.example.outer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyOuterBean {
 
+    @Value("${myprop.name}") // injecting values from properties
+    private String name;
+
+    @Value("${myprop.address: default address}") // default value
+    private String address;
+
     @Override
     public String toString() {
-        return "MyOuterBean{}";
+        return "MyOuterBean{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
